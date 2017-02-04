2d20d41df77a37bd6c8455d86c79b277a6063196
Haresh Khanna <khanna.haresh@yahoo.co.in>
2017-02-04 15:30:39 +0530

    Creates login screen

--- app/src/main/java/com/iitr/cfd/aasha/activities/LoginActivity.java
+++ app/src/main/java/com/iitr/cfd/aasha/activities/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.iitr.cfd.aasha.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.iitr.cfd.aasha.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        submitButton = (Button) findViewById(R.id.submit_button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
