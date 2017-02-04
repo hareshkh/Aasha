0ccb698ee47e51704e2f0c095cabc26e31e66003
Haresh Khanna <khanna.haresh@yahoo.co.in>
2017-02-04 16:24:36 +0530

    Adds content in HomeActivity

--- app/src/main/java/com/iitr/cfd/aasha/activities/HomeActivity.java
+++ app/src/main/java/com/iitr/cfd/aasha/activities/HomeActivity.java
@@ -1,15 +1,72 @@
 package com.iitr.cfd.aasha.activities;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.iitr.cfd.aasha.R;
+import com.iitr.cfd.aasha.fragments.AppointmentFragment;
+import com.iitr.cfd.aasha.fragments.HistoryFragment;
+import com.iitr.cfd.aasha.fragments.HospitalFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new AppointmentFragment(), getString(R.string.tab_one));
+        adapter.addFragment(new HospitalFragment(), getString(R.string.tab_two));
+        adapter.addFragment(new HistoryFragment(), getString(R.string.tab_three));
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
---
+++ app/src/main/java/com/iitr/cfd/aasha/fragments/AppointmentFragment.java
@@ -0,0 +1,26 @@
+package com.iitr.cfd.aasha.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.iitr.cfd.aasha.R;
+
+public class AppointmentFragment extends Fragment {
+
+    public AppointmentFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_appointments, container, false);
+    }
+}
